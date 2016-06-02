7b2ad0a661cbe70e2edfe5839f34947f7a4fb76c
Dmitry Koval <dkoval@heliostech.fr>
2016-06-02 18:21:11 +0300

    Added bootmaster kaigara example.

--- app/models/sysops/environment.rb
+++ app/models/sysops/environment.rb
@@ -6,6 +6,10 @@ module Kaigara
       @variables = {}
     end
 
+    def load_metadata
+      load 'metadata.rb'
+    end
+
     def method_missing(method_sym, *arguments, &block)
       @variables[method_sym.to_s] = arguments[0] rescue nil
     end
