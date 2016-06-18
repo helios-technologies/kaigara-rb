9d8d43c87ec77d28a2dc5046d4a0906fe6279bf9
Camille Meulien <camille.meulien@gmail.com>
2016-06-18 23:58:37 +0200

    Add redhat_family? and debian_family? block statement Add package method which run yum or apt-get depending on OS family Add package_update which run yum update or apt-get Add repo_extended method which install epel-release repository on CentOS

--- app/models/sysops/operation.rb
+++ app/models/sysops/operation.rb
@@ -7,6 +7,10 @@ module Kaigara
       include Thor::Actions
       include Thor::Shell
 
+      #
+      # What??
+      # TODO: add a comment
+      #
       no_commands do
         def inject(opts = {})
           opts.each do |k,v|
@@ -44,6 +48,74 @@ module Kaigara
 
       exit_status = wait_thr.value
       raise 'Non zero result' if exit_status != 0
+    end
+
+    #
+    # Return true if the `file` exists? and the content matches the `match`
+    # If a block is given it's executed if the statement is true
+    #
+    def file_matches?(file, match)
+      if File.exists?(file)
+        if File.read(file).match(match)
+          yield if block_given?
+          return true
+        end
+      end
+      false
+    end
+
+    #
+    # Return true if the OS is Ubuntu or Debian
+    # If a block is given it's executed if the statement is true
+    #
+    def debian_family?
+      file_matches?("/etc/issue", /Ubuntu|Debian/i) do
+        yield if block_given?
+      end
+    end
+
+    #
+    # Return true if the OS is CentOS or RedHat
+    # If a block is given it's executed if the statement is true
+    #
+    def redhat_family?
+      file_matches?("/etc/redhat-release", /CentOS|Red Hat/i) do
+        yield if block_given?
+      end
+    end
+
+    #
+    # Add common additional repositories
+    #
+    def repo_extended
+      redhat_family? do
+        package("epel-release")
+        package_update
+      end
+    end
+
+    #
+    # Update the local repository cache
+    #
+    def package_update
+      if debian_family?
+        execute("apt-get update")
+      elsif redhat_family?
+        execute("yum update")
+      end
+    end
+
+    #
+    # Install the package depending on OS
+    #
+    def package(name, version = nil)
+      if debian_family?
+        execute("apt-get install -y #{ name }")
+      elsif redhat_family?
+        execute("yum install -y #{ name }")
+      else
+        raise "OS not supported"
+      end
     end
 
     def template(name, target = nil)
