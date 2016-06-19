bbab7695624dd8f5a21fb7c8b52d267415f15056
Camille Meulien <camille.meulien@gmail.com>
2016-06-19 15:16:20 +0200

    Refactor multi os integration test

--- integration/operations-multi-os_spec.rb
+++ integration/operations-multi-os_spec.rb
@@ -1,24 +1,7 @@
 require_relative "spec_helper"
 
-describe "sysops exec" do
+describe "operations on any OS" do
   let(:sysops) { Kaigara::Sysops.new }
-
-  def exec_in_container(os)
-    cmd = "docker run -v #{KAIGARA_GEMPATH}:/root:ro --rm heliostech/kaigara:#{os} bash -c 'cd testops && ../bin/kaish sysops exec'"
-    puts "Running: #{ cmd }"
-    stdin, stdout, stderr, wait_thr = Open3.popen3({}, cmd)
-    Thread.new do
-      stdout.each { |l| puts l }
-    end
-    Thread.new do
-      stderr.each { |l| STDERR.puts l }
-    end
-    stdin.close
-    exit_status = wait_thr.value
-    if exit_status != 0
-      raise "Command #{ cmd } returned status code #{ exit_status }"
-    end
-  end
 
   describe 'exec' do
     before do
@@ -26,7 +9,7 @@ describe "sysops exec" do
       @old_pwd = Dir.pwd
       Dir.chdir 'testops'
 
-      File.write("operations/001_print.rb", <<EOF
+      File.write("operations/001_install_packages.rb", <<EOF
 debian_family? do
   execute("echo This is a debian or ubuntu")
 end
@@ -40,7 +23,6 @@ package_update
 package("htop")
 EOF
                 )
-
     end
 
     after do
--- integration/spec_helper.rb
+++ integration/spec_helper.rb
@@ -4,3 +4,20 @@ require 'open3'
 require 'kaigara'
 
 KAIGARA_GEMPATH = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+def exec_in_container(os)
+  cmd = "docker run -v #{KAIGARA_GEMPATH}:/root:ro --rm heliostech/kaigara:#{os} bash -c 'cd testops && ../bin/kaish sysops exec'"
+  puts "Running: #{ cmd }"
+  stdin, stdout, stderr, wait_thr = Open3.popen3({}, cmd)
+  Thread.new do
+    stdout.each { |l| puts l }
+  end
+  Thread.new do
+    stderr.each { |l| STDERR.puts l }
+  end
+  stdin.close
+  exit_status = wait_thr.value
+  if exit_status != 0
+    raise "Command #{ cmd } returned status code #{ exit_status }"
+  end
+end
