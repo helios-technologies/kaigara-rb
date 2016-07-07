e11165a9bd9b2372fb12cd8cfafc7e491eaffc71
Camille Meulien <camille.meulien@gmail.com>
2016-07-07 09:24:48 +0200

    Add the ability to not include Vagrantfile or Dockerfile.erb in `sysops create` process, enable both by default

--- spec/sysops_spec.rb
+++ spec/sysops_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 require 'byebug'
 
 describe Kaigara::Sysops do
-  let(:sysops) { Kaigara::Sysops }
+  let(:sysops_class) { Kaigara::Sysops }
+  let(:sysops) { sysops_class.new }
   let(:create_params) { [] }
 
   before(:each) do
     @src_home = Dir.pwd
-    sysops.start(['create', 'testops' ] + create_params)
+    sysops_class.start(['create', 'testops' ] + create_params)
     Dir.chdir 'testops'
   end
 
@@ -20,24 +21,34 @@ describe Kaigara::Sysops do
     context("default params") do
       it 'creates basic project' do
         root_files = Dir['*']
-        expect(root_files).to include("operations", "resources", "Vagrantfile", "metadata.rb")
-        expect(root_files).to_not include("Dockerfile.erb")
+        expect(root_files).to include("operations", "resources", "Vagrantfile", "metadata.rb", "Dockerfile.erb")
       end
     end
 
-    context("--with-docker") do
-      let(:create_params) { ["--with-docker"] }
+    context("--docker --no-vagrant") do
+      let(:create_params) { ["--docker", "--no-vagrant"] }
 
       it 'creates project with Dockerfile.erb' do
         root_files = Dir['*']
-        expect(root_files).to include("operations", "resources", "Vagrantfile", "metadata.rb", "Dockerfile.erb")
+        expect(root_files).to include("operations", "resources", "metadata.rb", "Dockerfile.erb")
+        expect(root_files).to_not include("Vagrantfile")
+      end
+    end
+
+    context("--no-docker --vagrant") do
+      let(:create_params) { ["--no-docker", "--vagrant"] }
+
+      it 'creates project with Vagrantfile' do
+        root_files = Dir['*']
+        expect(root_files).to include("operations", "resources", "metadata.rb", "Vagrantfile")
+        expect(root_files).to_not include("Dockerfile.erb")
       end
     end
   end
 
   describe 'generate' do
     it 'creates operations' do
-      sysops.new.generate 'print'
+      sysops.generate 'print'
       File.exist? Dir["operations/*_print.rb"].first
     end
   end
@@ -46,14 +57,14 @@ describe Kaigara::Sysops do
     context('run a simple ruby command') do
       it 'should run ruby code' do
         File.write("operations/001_print.rb", "print 'hello, kaigara!'")
-        expect { sysops.new.exec }.to output(/hello, kaigara!/).to_stdout
+        expect { sysops.exec }.to output(/hello, kaigara!/).to_stdout
       end
     end
 
     context('execute a simple shell command') do
       it 'should execute the command' do
         File.write("operations/001_shell_echo.rb", "execute('echo hello, kaigara')")
-        expect { sysops.new.exec }.to output(/hello, kaigara/).to_stdout
+        expect { sysops.exec }.to output(/hello, kaigara/).to_stdout
       end
     end
 
@@ -78,14 +89,14 @@ describe Kaigara::Sysops do
     context('execute a multiline shell') do
       it 'should execute each operation' do
         File.write("operations/001_multi_shell_echo.rb", "execute('echo hello\necho kaigara')")
-        expect { sysops.new.exec }.to output(/hello/).to_stdout
-        expect { sysops.new.exec }.to output(/kaigara/).to_stdout
+        expect { sysops.exec }.to output(/hello/).to_stdout
+        expect { sysops.exec }.to output(/kaigara/).to_stdout
       end
 
       it 'should fail at the first command which fail' do
         File.write("operations/001_multiline_shell_fail.rb",
                    "execute('echo hello\nfalse\necho kaigara')")
-        expect { sysops.new.exec }.to raise_error
+        expect { sysops.exec }.to raise_error
       end
     end
 
@@ -101,19 +112,19 @@ describe Kaigara::Sysops do
     end
 
     it 'should render the template' do
-      sysops.new.exec
+      sysops.exec
       expect(File.read("resources/script.sh")).to match(/echo 'bash'/)
     end
 
     it 'should make the script executable' do
-      sysops.new.exec
+      sysops.exec
       expect(File.stat('resources/script.sh').executable?).to be true
     end
 
     it 'should execute each script' do
-      expect { sysops.new.exec }.to output(/perl/).to_stdout
-      expect { sysops.new.exec }.to output(/bash/).to_stdout
-      expect { sysops.new.exec }.to output(/ruby/).to_stdout
+      expect { sysops.exec }.to output(/perl/).to_stdout
+      expect { sysops.exec }.to output(/bash/).to_stdout
+      expect { sysops.exec }.to output(/ruby/).to_stdout
     end
 
     after(:each) do
